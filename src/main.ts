75dbfd0747706900f6c5ef40ccf0bdaabf917bfe
Giancarlo <giancarlodev0705@gmail.com>
2024-08-06 14:58:23 -0700

    added vue-router and pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import "./styles/style.scss";
-
+import router from "./router";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(router);
+app.use(pinia);
+app.mount("#app");
